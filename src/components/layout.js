4817249500f4a0e97f97aa2b6c85a7a86255c734
jacobworrel <jacobworrel@gmail.com>
2019-02-18 22:55:31 -0800

    move global styles to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,13 +3,27 @@ import PropTypes from "prop-types"
 // import { StaticQuery, graphql } from "gatsby"
 import Nav from "./nav"
 import Social from './social';
-const Layout = ({ children }) => (
-  <div className="container">
-    <Nav/>
-    <div className="jumbotron">{children}</div>
-    <Social/>
-  </div>
-)
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '../css/styles.css';
+
+class Layout extends React.Component {
+  // https://github.com/gatsbyjs/gatsby/issues/11464
+  componentDidMount () {
+    require('bootstrap');
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <Nav/>
+        <div className="jumbotron">{this.props.children}</div>
+        <Social/>
+      </div>
+    );
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,10 @@ import Layout from "../components/layout"
 // import SEO from "../components/seo"
 import Intro from '../components/intro';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import '../css/styles.css';
-
-class IndexPage extends React.Component {
-  componentDidMount () {
-    require('bootstrap');
-  }
-
-  render () {
-    return (
-      <Layout>
-        <Intro/>
-      </Layout>
-    );
-  }
-}
+const IndexPage = () => (
+  <Layout>
+    <Intro/>
+  </Layout>
+);
 
 export default IndexPage
