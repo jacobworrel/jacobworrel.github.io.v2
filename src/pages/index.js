1e87428caa06286f4882636b35b2d944f3a3d371
jacobworrel <jacobworrel@gmail.com>
2019-02-18 22:21:13 -0800

    fix bootstrap import

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,22 @@ import Layout from "../components/layout"
 // import SEO from "../components/seo"
 import Intro from '../components/intro';
 
-import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '../css/styles.css';
 
-const IndexPage = () => (
-  <Layout>
-    <Intro/>
-  </Layout>
-);
+class IndexPage extends React.Component {
+  componentDidMount () {
+    require('bootstrap');
+  }
+
+  render () {
+    return (
+      <Layout>
+        <Intro/>
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage
