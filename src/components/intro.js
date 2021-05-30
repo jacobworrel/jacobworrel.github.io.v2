256273a94eaec42622181179c009e138ef2215f9
Jacob Worrel <jacob.worrel@innovid.com>
2021-05-30 13:38:33 -0700

    update intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -6,18 +6,21 @@ const Intro = () => (
     <div className="col-md-10 mr-auto">
       <p>Howdy ho! My name is Jacob.</p>
       <p>
-        I'm a software engineer specialized in full stack web development. I currently work on Conversion Logic's <a href="https://www.conversionlogic.com/platform/">marketing analytics platform</a>.
+        I'm a software engineer specialized in full stack web development.
+        I currently work on Innovid's <a href="https://www.innovid.com/marketer-solutions/measurement-solutions/">measurement products</a>,
+        building user interfaces and backend services that provide advertisers with valuable insights into campaign performance.
       </p>
       <p>
-        Recent interests include <a href="https://medium.com/@dabit3/full-stack-development-in-the-era-of-serverless-computing-c1e49bba8580">Serverless Computing</a> and <a href="https://elm-lang.org">Elm</a>.
+        Recent interests include functional programming and TypeScript.
       </p>
       <hr className="my-4"/>
       <p>
-        In my free time, I like to tinker on open source. I wrote a JavaScript library called <a href="http://ac-d3.com/">AC-D3</a> that renders
-        dynamic media visualizations inside of D3. The project was featured in <a href="http://javascriptweekly.com/issues/342">JavaScript Weekly</a>.
+        In my free time, I like to tinker on open source. I'm the lead developer and maintainer of <a href="https://www.npmjs.com/package/react-windowed-select">react-windowed-select</a>,
+        a virtualized select component with ~10K weekly downloads on NPM.
       </p>
       <p>
-        I also love <Link to="/writing">writing</Link>. I've contributed to <a href="https://medium.freecodecamp.org/">freeCodeCamp</a> and <a href="https://itnext.io/">ITNEXT</a>.
+        I also love <Link to="/writing">writing</Link>.
+        I've contributed to <a href="https://medium.freecodecamp.org/">freeCodeCamp</a> and <a href="https://itnext.io/">ITNEXT</a>.
       </p>
       <hr className="my-4"/>
       <p>
